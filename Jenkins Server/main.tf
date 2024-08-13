3de0f3932a641fdcaa405845569a911ff6a5097f
Sravani Mandela <162080576+SravaniMandela@users.noreply.github.com>
2024-08-13 18:47:04 +0530

    Add files via upload

---
+++ Jenkins Server/main.tf
@@ -0,0 +1,101 @@
+# VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jenkins-vpc"
+  cidr = var.vpc_cidr
+
+  azs            = data.aws_availability_zones.azs.names
+  public_subnets = var.public_subnets
+  map_public_ip_on_launch = true
+
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "jenkins-vpc"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+  public_subnet_tags = {
+    Name = "jenkins-subnet"
+  }
+}
+
+# SG
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "jenkins-sg"
+  description = "Security Group for Jenkins Server"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+
+# EC2
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "Jenkins-Server-new"
+
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.instance_key.key_name
+  monitoring                  = true
+  vpc_security_group_ids      = [module.sg.security_group_id]
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  user_data                   = file("jenkins-install.sh")
+  availability_zone           = data.aws_availability_zones.azs.names[0]
+
+  tags = {
+    Name        = "Jenkins-Server-new"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "instance_key" {
+  key_name   = "jenkins"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+resource "aws_secretsmanager_secret" "key_pair_secret_new" {
+  name = "key_pair_secret_jenkins"
+}
+resource "aws_secretsmanager_secret_version" "instance_key_version" {
+  secret_string = tls_private_key.ssh_key.private_key_pem
+  secret_id     = aws_secretsmanager_secret.key_pair_secret_new.id
+}
